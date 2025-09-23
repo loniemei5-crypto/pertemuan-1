28a6facbeedc796bd7478303139e6c81def284b3
loniemei5-crypto <loniemei5@gmail.com>
2025-09-23 10:20:42 +0800

    selesai

---
+++ meilonie.py
@@ -0,0 +1,6 @@
+n = int(input("masukan bilangan: "))
+
+if n % 2 == 0:
+    print("bilangan genap")
+else:
+    print("bilangan ganjil")
---
+++ p3.py
@@ -0,0 +1,8 @@
+harga = 10000000
+if harga => 10000000
+ diskon = harga - (harga*0.20)
+ print(diskon)
+else:
+ harga = 5000000
+ diskon = harga - (harga*0.10)
+ print(diskon)
